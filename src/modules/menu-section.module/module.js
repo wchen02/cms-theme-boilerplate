40e18cb3377c00306787ce6feb8ba473b882bb1b
Wensheng Chen <wchen@hubspot.com>
2020-05-14 00:53:37 -0400

    Format code with prettier

--- src/modules/menu-section.module/module.js
+++ src/modules/menu-section.module/module.js
@@ -1,10 +1,10 @@
 // Polyfill for NodeList.prototype.forEach() in IE
 if (window.NodeList && !NodeList.prototype.forEach) {
-  NodeList.prototype.forEach = function (callback, thisArg) {
-      thisArg = thisArg || window;
-      for (var i = 0; i < this.length; i++) {
-          callback.call(thisArg, this[i], i, this);
-      }
+  NodeList.prototype.forEach = function(callback, thisArg) {
+    thisArg = thisArg || window;
+    for (var i = 0; i < this.length; i++) {
+      callback.call(thisArg, this[i], i, this);
+    }
   };
 }
 
